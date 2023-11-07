dbe6a5190626a0ad0803270b3e3cfe9a9796eb9f
Federico Piedrasanta <fede.piedrasanta@gmail.com>
2023-11-07 16:36:12 -0300

    clase

--- js/bibliografia.js
+++ js/bibliografia.js
@@ -1,9 +1,9 @@
 class Bibliografia {
     constructor(bibliografia) {
         this.id = bibliografia.id;
-        this.urlImagen = bibliografia.Image?.url;
+        this.urlImagen = "https://localhost:7106" + bibliografia.link;
         this.titulo = bibliografia.title;
         this.descripcion = bibliografia.description;
-        this.urlDescarga = bibliografia.link;
+        this.urlDescarga = "https://localhost:7106" + bibliografia.link;
     }
 }
