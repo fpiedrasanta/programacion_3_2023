b3f9fa85d7d14cbd4f746c81bcaf7d69c008cfc1
fpiedrasanta <fede.piedrasanta@gmail.com>
2023-11-16 20:09:04 -0300

    paginado

--- js/pedido.js
+++ js/pedido.js
@@ -1,6 +1,6 @@
-async function getBibliografias(show) {
+async function getBibliografias(show, page, pageSize, query, orderBy) {
     try {
-        response = await fetch("https://localhost:7106/Resource/Type/1");
+        response = await fetch(`https://localhost:7106/Resource/Type/1?page=${page}&pageSize=${pageSize}&query=${query}&orderBy=${orderBy}`);
         json = await response.json();
         
         show(json);
--- js/recursos.js
+++ js/recursos.js
@@ -1,10 +1,56 @@
-getBibliografias(showBibliografia);
+const PAGE_SIZE = 2;
+
+getBibliografias(showBibliografia, 1, PAGE_SIZE, "", "Title");
+
+let listBibliografy = {};
+
+const firstBibliografy = document.getElementById("first_bibliografy");
+
+firstBibliografy.addEventListener("click", (evt) => {
+    getBibliografias(showBibliografia, 1, PAGE_SIZE, "", "Title");
+});
+
+const prevBibliografy = document.getElementById("prev_bibliografy");
+
+prevBibliografy.addEventListener("click", (evt) => {
+    getBibliografias(showBibliografia, listBibliografy.prevPage, PAGE_SIZE, "", "Title");
+});
+
+const nextBibliografy = document.getElementById("next_bibliografy");
+
+nextBibliografy.addEventListener("click", (evt) => {
+    getBibliografias(showBibliografia, listBibliografy.nextPage, PAGE_SIZE, "", "Title");
+});
+
+const lastBibliografy = document.getElementById("last_bibliografy");
+
+lastBibliografy.addEventListener("click", (evt) => {
+    getBibliografias(showBibliografia, listBibliografy.totalPage, PAGE_SIZE, "", "Title");
+});
 
 function showBibliografia(bibliografias) {
-    console.log(bibliografias);
-    
+    listBibliografy = bibliografias;
+
+    const pageBibliografy = document.getElementById("page_bibliografy");
+    pageBibliografy.innerText = listBibliografy.page;
+
+    if(!listBibliografy.hasNextPage) {
+        console.log("Ultima página");
+    }
+
+    if(!listBibliografy.hasPrevPage) {
+        console.log("Primera página");
+    }
+
+    nextBibliografy.disabled = !listBibliografy.hasNextPage;
+    lastBibliografy.disabled = !listBibliografy.hasNextPage;
+    prevBibliografy.disabled = !listBibliografy.hasPrevPage;
+    firstBibliografy.disabled = !listBibliografy.hasPrevPage;
+
     const listaBibliografia = document.getElementById("bibliografia_list");
-    for(const bibliografia of bibliografias) {
+
+    listaBibliografia.innerHTML = "";
+    for(const bibliografia of bibliografias.list) {
         const b = new Bibliografia(bibliografia);
         const template = getTemplate(b);
         listaBibliografia.append(template);
